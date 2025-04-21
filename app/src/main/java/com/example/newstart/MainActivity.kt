031127e034c3e13ec2046cd25c32d6f28ca15ab5
Dikra <Dikramfvdf>
2025-04-21 21:36:31 +0300

    first

--- app/src/main/java/com/example/newstart/MainActivity.kt
+++ app/src/main/java/com/example/newstart/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.newstart
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.newstart.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setupView()
+    }
+
+    fun setupView() {
+        binding?.nextActivity?.setOnClickListener {
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
